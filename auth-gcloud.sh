9ce3e82f1328da86c10b197fa4baaecef053a838
Tony Li <tonyli.lives@gmail.com>
2017-09-18 17:38:02 -0400

    refactor script

--- auth-gcloud.sh
+++ auth-gcloud.sh
@@ -2,11 +2,22 @@
 set -euo pipefail
 cd "$(cd "$(dirname "$0")"; cd ..; pwd)"
 
-CRED=${GCP_JSON_KEY:-${GOOGLE_CREDENTIALS:-${TOKEN}}}
-KEY=/tmp/gcloud.json
+main() {
+  CRED=${GCP_JSON_KEY:-${GOOGLE_CREDENTIALS:-${TOKEN}}}
+  KEY=/tmp/gcloud.json
 
-echo "$CRED" > "$KEY"
+  echo "$CRED" > "$KEY"
 
-gcloud auth activate-service-account --key-file "$KEY"
+  _trace gcloud auth activate-service-account --key-file "$KEY"
 
-rm "$KEY"
+  _trace rm "$KEY"
+}
+
+# Display the underlying command
+_trace() {
+  cmd=($@)
+  echo "$ ${cmd[*]}"
+  "${cmd[@]}"
+}
+
+main
